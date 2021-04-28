2c3e4ce5dc6443f7299564739ead6c77dbb147fc
ahmed_zaki0263 <ahmed.zaki0263@gmail.com>
2021-04-28 06:18:29 +0200

    add item controller and model. test store an item using postman.

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('items', 'ItemController@index')->name('items');
+
+Route::prefix('item')->group(function () {
+    Route::post('store', 'ItemController@store')->name('store');
+    Route::put('{id}', 'ItemController@update')->name('update');
+    Route::delete('{id}', 'ItemController@destroy')->name('destroy');
 });
